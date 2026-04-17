3a2d21165bcbf8fa8dd9a7e3725c26f265279fbe
m988ar <vborja2003@gmail.com>
2026-04-17 14:33:53 -0300

    funcion buscarNombre en 2da rama

--- ArregloDeNombres.C
+++ ArregloDeNombres.C
@@ -7,9 +7,12 @@
 
 void mostrarPersonas(char **nombres);
 
+void buscarNombre(char **nombres, int id);
+
 int main(){
     char buff [MAX];
     char *nombres [NOMBRES];
+    int id;
 
     for (int i = 0; i < 5; i++)
     {
@@ -22,6 +25,10 @@ int main(){
 
     mostrarPersonas(nombres);
 
+    printf("Ingrese el ID que quiere buscar: ");
+    scanf("%d", &id);
+    buscarNombre(nombres, id);
+
     return 0;
 }
 
@@ -30,4 +37,16 @@ void mostrarPersonas(char **nombres){
     {
         printf("\n%s\n",nombres[i]);
     }
+}
+
+void buscarNombre(char **nombres, int id){
+    for (int i = 0; i < NOMBRES; i++)
+    {
+        if (i == id)
+        {
+            printf("\nEl nombre de id %d es: %s\n",id, nombres[i]);
+            return ;
+        }
+    }
+    printf("\nNo se encontro el valor buscado\n");
 }
